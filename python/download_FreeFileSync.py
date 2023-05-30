e6c160cc240444a5f7e99d0cd8ffa13e71890d7b
Jan Caha <jan.caha@outlook.com>
2023-05-30 11:04:16 +0200

    update

--- python/download_FreeFileSync.py
+++ python/download_FreeFileSync.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 import requests
@@ -5,39 +7,66 @@ import re
 import subprocess
 import sys
 
-s = requests.Session()
+import util_functions
 
-url = 'https://freefilesync.org'
-r = s.get(url + "/download.php")
 
-soup = BeautifulSoup(r.text, "html.parser")
+def main():
+    s = requests.Session()
 
-links = soup.findAll("a")
+    url = "https://freefilesync.org"
+    r = s.get(url + "/download.php")
 
-tar_link = None
+    soup = BeautifulSoup(r.text, "html.parser")
 
-link: Tag
-version: str
-file: str
+    links = soup.findAll("a")
 
-for link in links:
-    href = link.get("href")
-    if isinstance(href, str):
-        match = re.match("/download/FreeFileSync_.+_Linux.tar.gz", href)
-        if match:
-            tar_link = match.group(0)
-            version = tar_link.split("_")[1]
-            file = tar_link.replace("/download/", "")
-            break
+    tar_link = None
 
-if version != sys.argv[2]:
-    subprocess.run(["wget", url + tar_link])
-    subprocess.run(["tar", "-xvf", file])
-    unzipped = file.replace("Linux", "Install")
-    unzipped = unzipped.replace("tar.gz", "run")
-    print(unzipped)
-    subprocess.run(["chmod", "+x", unzipped])
-    subprocess.run(["sudo", f"./{unzipped}"])
-    subprocess.run(["sed", "-i", f"s/^FreeFileSyncLastVersion=.*/FreeFileSyncLastVersion='{version}'/g", sys.argv[1]])
-else:
-    print("Skipping FreeFileSync instalation.")
+    link: Tag
+    version: str
+    file: str
+
+    util_functions.print_color(
+        "Checking out online version of FreeFileSync.",
+        util_functions.Colors.GREEN,
+    )
+
+    for link in links:
+        href = link.get("href")
+        if isinstance(href, str):
+            match = re.match("/download/FreeFileSync_.+_Linux.tar.gz", href)
+            if match:
+                tar_link = match.group(0)
+                version = tar_link.split("_")[1]
+                file = tar_link.replace("/download/", "")
+                break
+
+    existing_version = util_functions.get_install_variable("FreeFileSyncLastVersion")
+
+    util_functions.print_color(
+        f"\tOnline version: {version}. Version stored in file: {existing_version}.",
+        util_functions.Colors.GREEN,
+    )
+
+    if version != existing_version or not util_functions.binary_exist("freefilesync"):
+        subprocess.run(["wget", url + tar_link])
+        subprocess.run(["tar", "-xvf", file])
+        unzipped = file.replace("Linux", "Install")
+        unzipped = unzipped.replace("tar.gz", "run")
+        print(unzipped)
+        subprocess.run(["chmod", "+x", unzipped])
+        subprocess.run(["sudo", f"./{unzipped}"])
+        subprocess.run(
+            [
+                "sed",
+                "-i",
+                f"s/^FreeFileSyncLastVersion=.*/FreeFileSyncLastVersion='{version}'/g",
+                sys.argv[1],
+            ]
+        )
+    else:
+        util_functions.print_color("Skipping FreeFileSync installation.")
+
+
+if __name__ == "__main__":
+    main()
