3e7d4889be5692581bdba5bc582dcf86e9fe13da
Jan Caha <jan.caha@outlook.com>
2023-06-06 11:51:06 +0200

    update to allow closing

--- docker_run_pg.sh
+++ docker_run_pg.sh
@@ -1,7 +1,22 @@
 export HOME_DIR=~
 
-DOCKER_CONTAINER=$(docker ps -q -f name="postgis-machine")
-docker compose -f $HOME_DIR/Scripts/docker_compose/postgresql-postgis-compose.yaml up -d
+export CONTAINER_NAME=postgis-machine
+
+RUNS="$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME )"
+
+if [ $RUNS = "true" ]; then
+    while true; do
+        read -p "Close the docker machine? (y/n)" yn
+        case $yn in
+            [Yy]* ) docker compose -f $HOME_DIR/Scripts/docker_compose/postgresql-postgis-compose.yaml stop; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+else
+    docker compose -f $HOME_DIR/Scripts/docker_compose/postgresql-postgis-compose.yaml up -d
+fi
+
 # docker compose -f docker_compose/postgresql-postgis-compose.yaml stop
 
-sleep 5
+sleep 2
