6b241182ba3f28ec94444148f6c3acfec9b35d3b
Jan Caha <jan.caha@outlook.com>
2023-07-30 15:23:38 +0200

    update conda

--- update_and_upgrade.sh
+++ update_and_upgrade.sh
@@ -102,7 +102,9 @@ echo ""
 # conda update
 echo -e "$YELLOW---conda update---$NORMAL"
 source ~/miniconda3/etc/profile.d/conda.sh
-conda update -y -n base -c conda
+conda update -y conda
+# in case of issues maybe ownership of the miniconda install is the case
+# sudo chown -R $USER:$USER ~/miniconda3
 # conda update -y -n base -c conda conda-forge
 echo -e "$YELLOW---end conda update---$NORMAL"
 echo ""
