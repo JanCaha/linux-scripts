343db4f954a51cbedeb04545bae0afb4536acb5b
Jan Caha <jan.caha@outlook.com>
2023-09-29 13:48:06 +0200

    point qgis profiles to proper location

--- docker_run_qgis_latest.sh
+++ docker_run_qgis_latest.sh
@@ -6,6 +6,7 @@ docker build -t qgis-dev -f $BASEDIR/docker/QGIS-nigthly-unstable.dockerfile .
 xhost +
 docker run --rm -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /home/$USER/.local/share/QGIS/QGIS3:/root/.local/share/QGIS/QGIS3 \
     -v /home/$USER:/home/$USER \
     -p 127.0.0.1:5678:5678 \
     -e DISPLAY=unix$DISPLAY \
