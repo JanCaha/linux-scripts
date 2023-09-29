5bc706f6f73ff09b01a89cbb11759f3c4d9d5dc8
Jan Caha <jan.caha@outlook.com>
2023-09-29 13:45:13 +0200

    share whole user folder

--- docker_run_qgis_latest.sh
+++ docker_run_qgis_latest.sh
@@ -6,8 +6,7 @@ docker build -t qgis-dev -f $BASEDIR/docker/QGIS-nigthly-unstable.dockerfile .
 xhost +
 docker run --rm -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $DIR_CODES \
-    -v $DIR_QGIS_PROFILES \
+    -v /home/$USER:/home/$USER \
     -p 127.0.0.1:5678:5678 \
     -e DISPLAY=unix$DISPLAY \
     qgis-dev \
