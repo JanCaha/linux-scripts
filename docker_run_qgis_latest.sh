677660612a3ad02de5bf500153eb71e93cd9211d
Jan Caha <jan.caha@outlook.com>
2024-05-10 23:55:38 +0200

    fix ports

--- docker_run_qgis_latest.sh
+++ docker_run_qgis_latest.sh
@@ -8,7 +8,7 @@ docker run --rm -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /home/$USER/.local/share/QGIS/QGIS3:/root/.local/share/QGIS/QGIS3 \
     -v /home/$USER:/home/$USER \
-    -p 127.0.0.1:5678:5678 \
+    -p 5678:5678 \
     -e DISPLAY=unix$DISPLAY \
     qgis-dev \
     /bin/bash -c qgis
