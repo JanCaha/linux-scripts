341371d8db650081b462a809bd6797773f6e6c56
Jan Caha <jan.caha@outlook.com>
2024-06-08 15:05:01 +0200

    fix jellyfin

--- install/jellyfin.sh
+++ install/jellyfin.sh
@@ -1,10 +1,12 @@
 # Jellyfin
 sudo apt-get install -y jellyfin-server jellyfin-web jellyfin-ffmpeg6 
 
-sudo ln -s $HOME/Documents/jellyfin /var/lib/jellyfin
+sudo ln -s $HOME/Documents/jellyfin/var /var/lib/jellyfin
+sudo ln -s $HOME/Documents/jellyfin/etc/jellyfin /etc/jellyfin
 sudo chown -R jellyfin $HOME/Documents/jellyfin
 
-iptables -A INPUT -p tcp --dport 8096 -j ACCEPT 
+
+# sudo iptables -A INPUT -p tcp --dport 8096 -j ACCEPT
 
 
 # this does not work
