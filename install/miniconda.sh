d34bdc30ea2a10ec26dde3be395beb77da71bd04
Jan Caha <jan.caha@outlook.com>
2024-08-26 13:30:28 +0200

    replace miniconda by miniforge

--- install/miniconda.sh
+++
@@ -1,5 +0,0 @@
-# MiniConda
-cd /tmp
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh -b -f -p ~$USER/miniconda3
