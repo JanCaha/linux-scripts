3dc4c5250340a046b47458ebb61cb2ed80e3ae4d
Jan Caha <jan.caha@outlook.com>
2024-10-08 23:06:14 +0200

    use venv

--- run_onedrive_ui.sh
+++ run_onedrive_ui.sh
@@ -6,6 +6,19 @@ else
     cd OneDriveGUI
     git pull
 fi
-python3 -m pip install -r requirements.txt
+
+VENV_FOLDER=.venv
+if [ -d "$VENV_FOLDER" ];then
+    echo "VENV $VENV_FOLDER exists."
+else
+    echo "Creating the VENV $VENV_FOLDER."
+    python3 -m venv $VENV_FOLDER --symlinks --system-site-packages 
+fi
+
+. ~/Applications/OneDriveGUI/.venv/bin/activate
+echo "Python VENV activated."
+
+python -m pip install PySide6_Essentials requests
+
 cd src/
-python3 OneDriveGUI.py
+python OneDriveGUI.py
