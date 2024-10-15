8f5a7f75d81046586f1aeaddcb71b23e3a794f26
Jan Caha <jan.caha@outlook.com>
2024-10-15 14:27:19 +0200

    add packages to base env

--- install/miniforge.sh
+++ install/miniforge.sh
@@ -4,3 +4,24 @@ curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mi
 bash Miniforge3-$(uname)-$(uname -m).sh
 # conda init --reverse --dry-run
 # conda init --reverse
+
+conda activate base
+
+conda install -y \
+    beautifulsoup4 \
+    nbclient \
+    ipykernel \
+    pylint \
+    pycodestyle \
+    flake8 \
+    mypy \
+    black \
+    isort
+
+pip3 install \
+    ocrmypdf \
+    pytest-qgis \
+    mkdocs-bootswatch 
+    pb_tool \
+	https://codeload.github.com/mkdocs/mkdocs-bootstrap/zip/master \
+	git+https://github.com/it-novum/mkdocs-featherlight.git
