5a543f08301a587d03193cf57f65adeda3e34ae1
Jan Caha <jan.caha@outlook.com>
2025-02-05 22:22:09 +0100

    add prints

--- tools/gdal_compress_raster.sh
+++ tools/gdal_compress_raster.sh
@@ -1,1 +1,9 @@
+echo "🚀 GDAL Translating $1"
+
 gdal_translate -ot Float32 -co COMPRESS=DEFLATE -co PREDICTOR=3 -co ZLEVEL=9 -co BIGTIFF=YES $1 $2
+
+if [ $? -eq 0 ]; then
+    echo "✅ GDAL Translate successful"
+else
+    echo "❌ GDAL Translate failed"
+fi
