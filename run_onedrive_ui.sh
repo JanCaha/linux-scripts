b4eee68d0faaf66836a1bd9a0e9f9faf08b6d0ed
Jan Caha <jan.caha@outlook.com>
2025-02-09 23:43:36 +0100

    echo

--- run_onedrive_ui.sh
+++ run_onedrive_ui.sh
@@ -2,9 +2,11 @@ cd ~/Applications
 if [ ! -d "OneDriveGUI" ]; then
     git clone https://github.com/bpozdena/OneDriveGUI.git
     cd OneDriveGUI
+    echo "✅ OneDriveGUI cloned."
 else
     cd OneDriveGUI
     git pull
+    echo "✅ OneDriveGUI updated."
 fi
 
 VENV_FOLDER=.venv
@@ -13,10 +15,11 @@ if [ -d "$VENV_FOLDER" ];then
 else
     echo "Creating the VENV $VENV_FOLDER."
     python3 -m venv $VENV_FOLDER --symlinks --system-site-packages 
+    echo "✅ VENV created."
 fi
 
 . ~/Applications/OneDriveGUI/.venv/bin/activate
-echo "Python VENV activated."
+echo "✅ Python VENV activated."
 
 python -m pip install PySide6_Essentials requests
 
