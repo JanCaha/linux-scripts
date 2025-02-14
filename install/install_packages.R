e29b6cfea3e425194ecc70ee8584031432dd4d79
Jan Caha <jan.caha@outlook.com>
2025-02-14 10:53:51 +0100

    handle install path

--- install/install_packages.R
+++ install/install_packages.R
@@ -1,3 +1,20 @@
+# Check if the directory exists and create it if it does not
+if (Sys.getenv("R_LIBS_USER") != "") {
+    print(paste("R_LIBS_USER is set to: ", Sys.getenv("R_LIBS_USER")))
+
+    if (!dir.exists(Sys.getenv("R_LIBS_USER"))) {
+        dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
+        print("\tDirectory created")
+    }
+    {
+        print("\tDirectory already exists")
+    }
+}
+else {
+    print("R_LIBS_USER is not set")
+}
+
+# Install the required packages
 install.packages(c(
     "tidyverse", 
     "tidymodels",
