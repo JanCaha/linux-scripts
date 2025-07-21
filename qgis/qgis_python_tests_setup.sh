8f27d3846b8bb4f8551b0c3f817bfb318e46c0c5
Jan Caha <jan.caha@outlook.com>
2025-07-21 13:33:06 +0200

    use qgis tests setup script

--- qgis/qgis_python_tests_setup.sh
+++ qgis/qgis_python_tests_setup.sh
@@ -1,4 +1,17 @@
 #!/bin/zsh
-chmod +x $QGIS_BUILD_DIR/tests/env.sh
-source $QGIS_BUILD_DIR/tests/env.sh
-export LD_LIBRARY_PATH=$QGIS_BUILD_DIR/lib
+_srcdir=$QGIS_SOURCES_DIR
+_builddir=$QGIS_BUILD_DIR
+
+if expr "${_builddir}" : "@" >/dev/null || test -z "${_builddir}" ||
+    expr "${_srcdir}" : "@" >/dev/null || test -z "${_srcdir}"; then
+    echo "This File, it wasn't generated properly." >&2
+    return 1
+fi
+
+export srcdir=${_srcdir}
+export builddir=${_builddir}
+
+export QGIS_PREFIX_PATH=${builddir}/output
+export PYTHONPATH=${builddir}/output/python:$PYTHONPATH
+
+export LD_LIBRARY_PATH=${builddir}/output/lib:$LD_LIBRARY_PATH
