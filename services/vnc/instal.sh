072276684d906bb00531a12e7c0e4263616c1f4c
Jan Caha <jan.caha@outlook.com>
2025-10-27 19:52:29 +0100

    vnc

---
+++ services/vnc/instal.sh
@@ -0,0 +1,11 @@
+sudo apt install x11vnc -y
+x11vnc -storepasswd
+
+
+ps -ef | grep -E 'X|Xorg'
+
+cp x11vnc.service /etc/systemd/system/x11vnc.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable x11vnc.service
+sudo systemctl start x11vnc.service
