a3fe78a9196f25bbe0def2fd68dc1960dd3feb2e
Jan Caha <jan.caha@outlook.com>
2025-11-08 00:09:00 +0100

    add edit data

--- install/jellyfin.sh
+++ install/jellyfin.sh
@@ -18,3 +18,14 @@ fi
 sudo systemctl start jellyfin
 
 sudo usermod -aG $USER jellyfin
+
+
+sudo sqlite3  $HOME/Backup/Jellyfin/var/data/jellyfin.db
+
+## SELECT Path FROM BaseItems WHERE Path LIKE '/Old/Path/%';
+
+UPDATE BaseItems
+SET Path = REPLACE(Path, '/Old/Path/', '/New/Path/')
+WHERE Path LIKE '/Old/Path/%';
+
+.exit
