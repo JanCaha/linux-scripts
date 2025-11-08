959d16ccc6170804081a0e463df7fdbe825221b7
Jan Caha <jan.caha@outlook.com>
2025-11-08 11:46:02 +0100

    add ufw

--- install/jellyfin.sh
+++ install/jellyfin.sh
@@ -1,21 +1,8 @@
 # Jellyfin
 sudo apt-get install -y jellyfin-server jellyfin-web jellyfin-ffmpeg6
 
-sudo systemctl stop jellyfin
-
-sudo rm -rf /var/lib/jellyfin
-sudo rm -rf /etc/jellyfin
-
-BACKUP_DIR=/home/cahik/Disc-Seagate/Backup
-
-if [ -d "$BACKUP_DIR/jellyfin" ]; then
-    sudo mkdir -p /var/lib/jellyfin
-    sudo mkdir -p /etc/jellyfin
-    sudo cp -r $BACKUP_DIR/jellyfin/var/ /var/lib/jellyfin/
-    sudo cp -r $BACKUP_DIR/jellyfin/etc/ /etc/jellyfin/
-fi
-
-sudo systemctl start jellyfin
+sudo ufw allow jellyfin
+sudo ufw reload
 
 sudo usermod -aG $USER jellyfin
 
