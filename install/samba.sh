af8f00842952806aa8a1d9aec117fd80baff96ab
Jan Caha <jan.caha@outlook.com>
2025-11-08 11:41:12 +0100

    add steps

--- install/samba.sh
+++ install/samba.sh
@@ -1,3 +1,25 @@
 # Samba
 sudo apt-get -y install samba 
 sudo usermod -aG sambashare $USER
+
+sudo nano /etc/samba/smb.conf
+
+## add at the end of the file:
+# [shared_folder]
+#    comment = Home Media Share Folder
+#    path = /Shared_Folder
+#    browseable = yes
+#    writable = yes
+#    read only = no
+#    create mask = 0664
+#    directory mask = 0775
+#    valid users = sambashare
+
+sudo smbpasswd -a sambashare
+sudo smbpasswd -e sambashare
+
+sudo systemctl restart smbd nmbd
+sudo systemctl enable smbd nmbd
+
+sudo ufw allow samba
+sudo ufw reload
