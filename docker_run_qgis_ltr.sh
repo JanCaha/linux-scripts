f2382197acb106c552e2fc39fd4ecbbba1355d25
Jan Caha <jan.caha@outlook.com>
2025-11-12 21:41:18 +0100

    update to use user

--- docker_run_qgis_ltr.sh
+++ docker_run_qgis_ltr.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 BASEDIR=$(dirname "$0")
 
-docker build -t qgis-ltr -f $BASEDIR/docker/QGIS-ltr.dockerfile $BASEDIR/docker
+docker build \
+    -t qgis-ltr \
+    -f $BASEDIR/docker/QGIS-ltr.dockerfile $BASEDIR/docker
 
 xhost +
 docker run --rm -it \
+    --user $(id -u):$(id -g) \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $DIR_WORK \
     -v $DIR_CODES \
@@ -15,4 +18,4 @@ docker run --rm -it \
     /bin/bash -c qgis
 xhost -
 
-sudo chown -R $USER /home/$USER/.local/share/QGIS
+# sudo chown -R $USER /home/$USER/.local/share/QGIS
