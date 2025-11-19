408b580ad5514ded245c7d8087757b9e3671f835
Jan Caha <jan.caha@outlook.com>
2025-11-19 11:02:44 +0100

    update

--- qgis/qgis_move_branch_to_folder.sh
+++ qgis/qgis_move_branch_to_folder.sh
@@ -2,35 +2,33 @@
 
 # Check if first argument is provided
 if [ -z "$1" ]; then
-    echo "Error: Branch name is required"
-    echo "Usage: $0 <branch_name>"
+    echo "❌ Error: Branch name is required"
+    echo "📖 Usage: $0 <branch_name>"
     exit 1
 fi
 
 BRANCH="$1"
 
-cd ~/QGIS
+cd $QGIS_SOURCES_DIR
 
 # Check if branch already exists
 if git show-ref --verify --quiet "refs/heads/$BRANCH"; then
-    echo "Error: Branch '$BRANCH' already exists"
-    exit 1
+    echo "✅ Branch '$BRANCH' already exists"
+else
+    echo "🚀 Creating branch '$BRANCH'"
+    git checkout -b "$BRANCH"
+    git checkout master
 fi
-
-MAIN_FOLDER="/QGIS"
 
 # Or check first:
-if [ ! -d "$MAIN_FOLDER" ]; then
-    sudo mkdir -p "$MAIN_FOLDER"
-    sudo chown -R $USER:$USER "$MAIN_FOLDER"
+if [ ! -d "$QGIS_WORKTREE_FOLDER" ]; then
+    sudo mkdir -p "$QGIS_WORKTREE_FOLDER"
+    sudo chown -R $USER:$USER "$QGIS_WORKTREE_FOLDER"
 fi
 
-git checkout -b "$BRANCH"
-git checkout master
-
-git worktree add "$MAIN_FOLDER/$BRANCH" "$BRANCH"
+git worktree add "$QGIS_WORKTREE_FOLDER/$BRANCH" "$BRANCH"
 
 # copy necessary files to the new worktree
-cp -r ./build-qt "$MAIN_FOLDER/$BRANCH/"
-cp -r ./.vscode "$MAIN_FOLDER/$BRANCH/"
-cp -r ./CMakePresets.json "$MAIN_FOLDER/$BRANCH/CMakePresets.json"
+echo "📂 Copy files not part of the git repository to the new worktree"
+cp -r ./.vscode "$QGIS_WORKTREE_FOLDER/$BRANCH/"
+cp -r ./CMakePresets.json "$QGIS_WORKTREE_FOLDER/$BRANCH/CMakePresets.json"
