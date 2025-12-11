26220afe3f43701f7cdb66adda33dc4ba7af64e3
Jan Caha <jan.caha@outlook.com>
2025-12-11 10:38:22 +0100

    build process

---
+++ qgis/qgis_update_build_latest.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+SCRIPT_DIR=$(dirname "$0")
+
+cd $QGIS_SOURCES_DIR
+
+cp -f $SCRIPT_DIR/CMakePresets.json $QGIS_SOURCES_DIR/CMakePresets.json
+
+echo "🚀 Configuring latest QGIS with GDAL from /opt/gdal, PDAL and with tests"
+
+cmake --preset=configure_with_gdal_312_no_tests 
+
+echo "🚀 Building latest QGIS with GDAL from /opt/gdal, PDAL and with tests"
+
+cmake --build --preset=build_with_GDAL_312_PDAL_tests
+
+# cmake --workflow --preset=test
+
+echo "✅ Latest QGIS build completed"
