78ff99668053751098b1abe2cf3ab2eec65f71e4
Jan Caha <jan.caha@outlook.com>
2026-01-06 08:49:02 +0100

    move from docker to conda

---
+++ qgis/run_qgis_ltr.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+QGIS_VERSION=3.40
+
+# check that conda is available
+if ! command -v conda &> /dev/null; then
+    echo "Error: conda is not available."
+    exit 1
+fi
+
+# check that specific env exist
+if ! conda info --envs | grep -q "^qgis-ltr"; then
+    echo "Creating conda environment qgis-ltr..."
+    conda create -y -n qgis-ltr -c conda-forge qgis=$QGIS_VERSION
+    conda activate qgis-ltr
+else
+    conda activate qgis-ltr
+    # check qgis version
+    if ! qgis --version | grep -q "$QGIS_VERSION"; then
+        echo "Updating qgis-ltr environment to QGIS version $QGIS_VERSION..."
+        conda install -y -c conda-forge qgis=$QGIS_VERSION
+    fi
+fi
+
+qgis
