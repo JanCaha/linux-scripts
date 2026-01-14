61c737de9943c908f829b6c8a027f206692cb74d
Jan Caha <jan.caha@outlook.com>
2026-01-14 11:44:25 +0100

    drivers

---
+++ install/drivers-nvidia.sh
@@ -0,0 +1,17 @@
+# NVIDIA Drivers
+# add contrib and non-free to /etc/apt/sources.list into trixie, trixie-security, trixie-updates
+sudo apt install linux-headers-$(uname -r)
+sudo apt install linux-headers-amd64
+sudo apt install nvidia-driver
+
+# Enable DRM modesetting for NVIDIA
+echo "options nvidia-drm modeset=1" | sudo tee /etc/modprobe.d/nvidia-drm.conf
+
+# Add nvidia modules to initramfs
+echo "nvidia" | sudo tee -a /etc/initramfs-tools/modules
+echo "nvidia_modeset" | sudo tee -a /etc/initramfs-tools/modules
+echo "nvidia_drm" | sudo tee -a /etc/initramfs-tools/modules
+echo "nvidia_uvm" | sudo tee -a /etc/initramfs-tools/modules
+
+# Update initramfs
+sudo update-initramfs -u
