c61e335e0adaad9138bdde164719926487ec41f4
Jan Caha <jan.caha@outlook.com>
2026-02-02 12:46:10 +0100

    update

--- install/drivers-nvidia.sh
+++ install/drivers-nvidia.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
 # NVIDIA Drivers
 # add contrib and non-free to /etc/apt/sources.list into trixie, trixie-security, trixie-updates
-sudo apt install linux-headers-$(uname -r)
-sudo apt install linux-headers-amd64
-sudo apt install nvidia-driver
+sudo apt install -y linux-headers-"$(uname -r)"
+sudo apt install -y linux-headers-amd64
+sudo apt install -y nvidia-driver
 
 # Enable DRM modesetting for NVIDIA
 echo "options nvidia-drm modeset=1" | sudo tee /etc/modprobe.d/nvidia-drm.conf
