8e66c34e14d7e7b308734b49999a43c8c2059d7d
Jan Caha <jan.caha@outlook.com>
2026-02-28 13:55:32 +0100

    update docker

--- complete_install.sh
+++ complete_install.sh
@@ -129,9 +129,17 @@ sudo apt-get install -y redshift
 sudo apt-get -y install git git-crypt
 
 # Docker
-sudo apt-get install -y \
-    docker.io \
-    docker-compose
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/debian \
+  trixie stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker || true
 if [[ -n "${USER:-}" ]]; then
