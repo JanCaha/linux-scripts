c62a64c4d66ddfc4e6686353ab0bb3c418dca69b
Jan Caha <jan.caha@outlook.com>
2026-03-01 11:28:06 +0100

    fix install

--- install/dbeaver.sh
+++ install/dbeaver.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+START_DIR="$(pwd)"
+
+# DBeaver
 cd /tmp
+
 gh release download --repo dbeaver/dbeaver --pattern "*.deb" --clobber
-sudo dpkg -i dbeaver-ce_*.deb
-sudo apt-get install -f -y
+
+sudo apt install "./$(ls dbeaver-ce*x86_64.deb)"
+
 rm dbeaver-ce_*.deb
+
+cd "$START_DIR"
--- install/xnview.sh
+++ install/xnview.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+START_DIR="$(pwd)"
+
 cd /tmp
+
 wget -O XnViewMP-linux-x64.deb "https://www.xnview.com/download.php?update=1&file=XnViewMP-linux-x64.deb"
-sudo gdebi XnViewMP-linux-x64.deb
+
+sudo apt install "./XnViewMP-linux-x64.deb"
+
+cd "$START_DIR"
