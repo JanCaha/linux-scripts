4ea7270316653fc7412a51ab922bbab4d5e7eb20
Jan Caha <jan.caha@outlook.com>
2026-03-14 12:57:33 +0100

    set env vars in tasks.json instead of script

--- qgis/vscode/run_test.sh
+++ qgis/vscode/run_test.sh
@@ -3,12 +3,6 @@
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export PYTHONPATH="$SCRIPT_DIR/../build/output/python:$SCRIPT_DIR/../build/output/python/plugins:$SCRIPT_DIR/../tests/src/python"
-export LD_LIBRARY_PATH="$SCRIPT_DIR/../build/output/lib:${LD_LIBRARY_PATH}"
-export QGIS_PREFIX_PATH="$SCRIPT_DIR/../build/output"
-export QT_QPA_PLATFORM=offscreen
-export QGIS_DEBUG=1
-
 cd build
 filename=$(basename "$1")
 module=$(basename "$(dirname "$1")")
