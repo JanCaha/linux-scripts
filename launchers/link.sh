c96b374215447d633af977aaff8d2e7b5f1e36cf
Jan Caha <jan.caha@outlook.com>
2026-03-17 10:19:18 +0100

    qgis launcher for /opt

--- launchers/link.sh
+++ launchers/link.sh
@@ -14,6 +14,7 @@ ln -sf "$SCRIPT_DIR/reboot-to-win.desktop" "$LAUNCHER_PATH/reboot-to-win.desktop
 ln -sf "$SCRIPT_DIR/update-upgrade.desktop" "$LAUNCHER_PATH/update-upgrade.desktop"
 ln -sf "$SCRIPT_DIR/connect-to-pc.desktop" "$LAUNCHER_PATH/connect-to-pc.desktop"
 ln -sf "$SCRIPT_DIR/qgis-dev.desktop" "$LAUNCHER_PATH/qgis-dev.desktop"
+ln -sf "$SCRIPT_DIR/qgis.desktop" "$LAUNCHER_PATH/qgis.desktop"
 
 if command -v update-desktop-database >/dev/null 2>&1; then
 	update-desktop-database "$LAUNCHER_PATH"
