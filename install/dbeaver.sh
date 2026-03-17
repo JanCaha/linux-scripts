2c07ee7385bdc82ee5f0ec4c27eddaf29909c8d2
Jan Caha <jan.caha@outlook.com>
2026-03-17 09:18:40 +0100

    fix

--- install/dbeaver.sh
+++ install/dbeaver.sh
@@ -10,7 +10,7 @@ LATEST_VERSION=$(gh release view --repo dbeaver/dbeaver --json tagName --jq .tag
 
 CURRENT_VERSION=""
 if command -v dbeaver >/dev/null 2>&1; then
-	CURRENT_VERSION=$(dbeaver --version 2>/dev/null | sed -nE 's/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/p' || echo "")
+	CURRENT_VERSION=$(dbeaver --version 2>/dev/null | sed -nE 's/^[^0-9]*(([0-9]+\.){2}[0-9]+)(\.[0-9]+)*$/\1/p' || echo "")
 fi
 
 if [[ -n "$LATEST_VERSION" && -n "$CURRENT_VERSION" && "$LATEST_VERSION" == "$CURRENT_VERSION" ]]; then
@@ -22,7 +22,7 @@ else
 
 	sudo apt install "./$(ls dbeaver-ce*x86_64.deb)"
 
-	rm dbeaver-ce_*.deb
+	rm dbeaver-ce*.deb
 fi
 
 cd "$START_DIR"
