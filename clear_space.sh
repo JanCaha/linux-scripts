2034815c9a9a9e2e63afcf5fc6929c8769864f20
Jan Caha <jan.caha@outlook.com>
2026-06-18 13:45:51 +0200

    add options

--- clear_space.sh
+++ clear_space.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Clear APT cache
+sudo apt clean
+sudo apt autoremove -y
+
+# Other cache cleaners
 conda clean --all --yes
 pip cache purge
 uv cache clean
 rm -rf ~/.cache/gdb
-rm -rf ~/.cache/vscode-cpptools/*  
+rm -rf ~/.cache/vscode-cpptools/*
+ccache --clear
+  
