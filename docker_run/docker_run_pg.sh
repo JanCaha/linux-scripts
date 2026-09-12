e1704bbc962bb87ffc241151d2bc7d568ffcba5a
Jan Caha <jan.caha@outlook.com>
2026-09-12 09:26:13 +0200

    restyle

--- docker_run/docker_run_pg.sh
+++ docker_run/docker_run_pg.sh
@@ -2,18 +2,22 @@
 BASEDIR=$(dirname "$0")
 
 source $BASEDIR/docker_envs.sh
+source $HOME/.zshenv
 
 export CONTAINER_NAME=postgis-machine
 
-RUNS="$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME )"
+RUNS="$(docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME)"
 
 if [ $RUNS = "true" ]; then
     while true; do
         read -p "Close the docker machine? (y/n)" yn
         case $yn in
-            [Yy]* ) docker compose -f $DOCKER_COMPOSE/postgresql-postgis.yaml stop; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
+            [Yy]*)
+                docker compose -f $DOCKER_COMPOSE/postgresql-postgis.yaml stop
+                break
+                ;;
+            [Nn]*) exit ;;
+            *) echo "Please answer yes or no." ;;
         esac
     done
 else
