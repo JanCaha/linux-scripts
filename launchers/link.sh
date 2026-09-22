40b097969725a502dd0b7b9b68050796a9df0fd4
Jan Caha <jan.caha@lutraconsulting.co.uk>
2026-09-22 11:23:57 +0200

    fix

--- launchers/link.sh
+++ launchers/link.sh
@@ -4,11 +4,7 @@ set -euo pipefail
 # get current script directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# this copies .desktop files to $HOME/.local/share/applications, substituting
-# __HOME__ and __QGIS_BUILD_DIR__ placeholders with their actual values
-LAUNCHER_PATH="$HOME/.local/share/applications"
-
-mkdir -p "$LAUNCHER_PATH"
+[ -d "$LAUNCHER_PATH" ] || mkdir -p "$LAUNCHER_PATH"
 
 for desktop_file in "$SCRIPT_DIR"/*.desktop; do
     cp -f "$desktop_file" "$LAUNCHER_PATH/$(basename "$desktop_file")"
